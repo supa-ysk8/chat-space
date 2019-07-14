df709bd5464260b287780e5eeee79903811434b6
supa-ysk8 <supa.ysk8.19@gmail.com>
2019-07-15 01:41:43 +0900

    chat-space__incremental-search-first-part

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,42 @@
-$(function() {
-  $("")
-})
+$(document).on('turbolinks:load', function(){
+
+    var search_list = $(".user-search-result");
+
+    function appendUser(user) {
+        var html = `<div class="chat-group-user clearfix" id="chat-group-user-${user.id}">
+                      <p class="chat-group-user__name">${user.name}</p>
+                      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                    </div>`
+        search_list.append(html);
+    }
+
+    function appendErrMsgToHTML(user) {
+      var html = ``
+      search_list.append(html);
+    }
+
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $(".user-search-result").empty();
+      if (users.length !== 0 && input.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+});
