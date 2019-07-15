6ff74626e23fcbca559b696b5868abe2904025d1
supa-ysk8 <supa.ysk8.19@gmail.com>
2019-07-15 18:22:34 +0900

    incremental-search__edit

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,42 +1,72 @@
 $(document).on('turbolinks:load', function(){
-
-    var search_list = $(".user-search-result");
-
-    function appendUser(user) {
-        var html = `<div class="chat-group-user clearfix" id="chat-group-user-${user.id}">
-                      <p class="chat-group-user__name">${user.name}</p>
-                      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
-                    </div>`
-        search_list.append(html);
+  var search_list = $('#user-search-result');
+  
+  function appendUser(user) {
+    // console.log(user)
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id=${user.id}' data-user-name='${user.name}'>追加</a>
+                </div>`
+    search_list.append(html);
+  }
+  
+  function appendNoUser(user) {
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user}</p>
+                </div>`
+    search_list.append(html);
+  }
+  
+  $('#user-search-field').on('keyup', function () {
+    var input = $('#user-search-field').val();
+    // console.log(input)
+    if (input !== "") {
+      $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: {
+            keyword: input
+          },
+          dataType: 'json'
+        })
+        
+        .done(function (users) {
+          $('#user-search-result').empty();
+          
+          if (users.length !== 0) {
+            users.forEach(function (user) {
+              appendUser(user);
+            });
+          } else {
+            appendNoUser('一致るするユーザーはありません');
+          }
+        })
+        
+        .fail(function () {
+          alert('ユーザーの検索に失敗しました');
+        })
     }
-
-    function appendErrMsgToHTML(user) {
-      var html = ``
-      search_list.append(html);
-    }
-
-  $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-
-    .done(function(users) {
-      $(".user-search-result").empty();
-      if (users.length !== 0 && input.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致するユーザーが見つかりません");
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    })
+  });
+  
+  function addUser(user_id, user_name) {
+    var html = `<div class='chat-group-user clearfix' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                    <p class='chat-group-user__name'>${user_name}</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $('#chat-group-users').append(html);
+  }
+  
+  $('#user-search-result').on('click', '.chat-group-user__btn--add', function () {
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    // console.log(user_id,user_name)
+    addUser(user_id, user_name);
+    // console.log(user_id,user_name)
+    $(this).parent().remove();
+  });
+  
+  $('#chat-group-users').on('click', '.chat-group-user__btn--remove', function () {
+    $(this).parent().remove();
   });
 });
